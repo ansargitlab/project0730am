d4ea65d417593abb95366d9d16543f0b94ceab37
root <root@ip-10-0-16-254.us-east-2.compute.internal>
2025-06-26 18:09:20 +0000

    bug remove1

--- app.py
+++ app.py
@@ -5,3 +5,4 @@ http.createServer(function (req, res) {
   res.write('Welcome to DevOps Training'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
+1
